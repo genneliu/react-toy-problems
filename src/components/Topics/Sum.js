da2a75f1e1fae4db7a6d0b3d201fb8cffc1b2b2f
Genne Liu <genneliu@gmail.com>
2021-10-20 17:10:23 -0700

    working on sum

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,7 +1,42 @@
-import React, {component} from 'react';
+import React, {Component} from 'react';
 
-function Sum() {
-    return <p> Sum </p>
+
+export default class Sum extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            number1: 0,
+            number2: 0,
+            sum: null
+        }
+    }
+
+    handleChange(val) {
+        this.setState({ number1: val })
+    }
+
+    handleChange2(val) {
+        this.setState({ number2: val })
+    }
+
+   calculateSum(number1, number2) {
+       let parse1 = parseInt(this.state.number1)
+       let parse2 = parseInt(this.state.number2)
+       
+        let sumBoth = parse1+parse2
+        this.setState({sum: sumBoth})
+    }
+
+    render() {
+        return (
+            <div className="puzzleBox sumPB">
+                <h4>Sum</h4>
+                <span className="resultsBox"> The Sum is: { JSON.stringify(this.state.sum, null, 10)}</span>
+                <input className="inputLine" onChange={ (e, f) => this.handleChange(e.target.value)}></input>
+                <button className="confirmationButton" onClick={ () => {this.calculateSum(this.state.number1, this.state.number2, 10)}}></button>
+            </div>
+        )
+    }
 };
-
-export default Sum;
